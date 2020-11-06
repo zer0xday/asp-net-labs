d99fd08f74abb9559b6ad912eff47b5f626d3aa6
zer0xday <majcher10@gmail.com>
2020-11-06 20:01:27 +0100

    Task 2nd solved

---
+++ asp-net-labs/Controllers/ApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using asp_net_labs.Models;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    [ApiController]
+    [Route("api/company")]
+    public class ApiController : ControllerBase
+    {
+        public Dictionary<string, object> Post(CompanyModel company)
+        {
+            var response = new Dictionary<string, object>();
+            bool succeded = true;
+
+            try
+            {
+                var viewModel = new CompanyAddedViewModel
+                {
+                    NumberOfCharsInName = company.Name.Length,
+                    NumberOfCharsInDescription = company.Description.Length,
+                    IsHidden = !company.IsVisible
+                };
+                response.Add("data", viewModel);
+            }
+            catch (Exception exception)
+            {
+                succeded = false;
+                response.Add("errorMsg:", exception.Message);
+            }
+
+            response.Add("success", succeded);
+
+            return response;
+        }
+    }
+}
---
+++ asp-net-labs/wwwroot/js/addNewItem.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    const alertElement = document.getElementById("success-alert");
+    const formElement = document.forms[0];
+
+    const addNewItem = async () => {
+        // 1. read data from the form
+        const endpoint = 'api/company';
+        const requestData = JSON.stringify({
+            Name: formElement.querySelector('input[name="Name"]').value,
+            Description: formElement.querySelector('textarea[name="Description"]').value,
+            isVisible: formElement.querySelector('input[name="IsVisible"]').checked
+        });
+        const config = {
+            method: 'POST',
+            body: requestData,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'RequestVerificationToken': formElement.querySelector('input[name="__RequestVerificationToken"]')
+            }
+        };
+
+        //// 2. call the application server using fetch method
+        const response = await fetch(endpoint, config);
+        const responseJson = await response.json();
+
+        if (responseJson.success) {
+            // 3. un-hide the alertElement when the request has been successful
+            alertElement.style.display = 'flex';
+        }
+    };
+    window.addEventListener("load", () => {
+        formElement.addEventListener("submit", event => {
+            event.preventDefault();
+            addNewItem().then(() => console.log("added successfully"));
+        });
+    });
+})();
